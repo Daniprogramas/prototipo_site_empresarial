a0e8a880b457467b30deee5d77e49acbff76b241
Danielle Morais <111515555+Daniprogramas@users.noreply.github.com>
2025-10-09 10:30:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function mostrarContato() {
-  alert("Entre em contato pelo e-mail: contato@empresaexemplo.com");
+  alert("Entre em contato pelo e-mail: contato@danisoftweb.com");
 }
 
 function alternarTema() {
@@ -8,4 +8,9 @@ function alternarTema() {
   document.querySelectorAll("nav a").forEach(el => el.classList.toggle("dark-mode"));
   document.querySelectorAll("button").forEach(el => el.classList.toggle("dark-mode"));
   document.querySelector("footer").classList.toggle("dark-mode");
+}
+
+function mostrarAba(id) {
+  document.querySelectorAll(".tab-content").forEach(el => el.style.display = "none");
+  document.getElementById(id).style.display = "block";
 }
