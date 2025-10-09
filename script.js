0c5613daa195a81fb4c0d94ccb91a0d64aaa633d
Danielle Morais <111515555+Daniprogramas@users.noreply.github.com>
2025-10-09 10:07:53 -0300

    Update script.js
    Função JS para alternar tema

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function mostrarContato() {
   alert("Entre em contato pelo e-mail: contato@empresaexemplo.com");
 }
+function alternarTema() {
+  document.body.classList.toggle("dark-mode");
+  document.querySelector("header").classList.toggle("dark-mode");
+  document.querySelectorAll("nav a").forEach(el => el.classList.toggle("dark-mode"));
+  document.querySelector("button").classList.toggle("dark-mode");
+  document.querySelector("footer").classList.toggle("dark-mode");
+}
