b7c4ea4460538e50b3a1265c6b9528b3165cfb50
Danielle Morais <111515555+Daniprogramas@users.noreply.github.com>
2025-10-10 11:17:32 -0300

    Create tema.js

---
+++ js/tema.js
@@ -0,0 +1,8 @@
+function alternarTema() {
+  const body = document.body;
+  const botao = document.getElementById("tema-btn");
+
+  body.classList.toggle("dark-mode");
+
+  botao.textContent = body.classList.contains("dark-mode") ? "☀️" : "🌙";
+}
