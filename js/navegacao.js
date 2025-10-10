69b23dfb64e684ce364da54804ec44134c5bde74
Danielle Morais <111515555+Daniprogramas@users.noreply.github.com>
2025-10-10 11:31:28 -0300

    Update navegacao.js

--- js/navegacao.js
+++ js/navegacao.js
@@ -1,9 +1,5 @@
-function mostrarContato() {
-  alert("Entre em contato pelo e-mail: danielle.morais940@gmail.com");
-}
-
 function carregarSecao(secao) {
-  fetch(`sections/${secao}.html`)
+  fetch(`../sections/${secao}.html`)
     .then(response => response.text())
     .then(html => {
       const conteudo = document.getElementById("conteudo");
